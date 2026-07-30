c70205391b2fae1826c8ab44f380ff83ccff7173
yhprasad <yhprasadhew@gmail.com>
2026-07-30 16:09:31 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-500">
+        Tailwind CSS v3 is Working 🚀
+      </h1>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./src/components/**/*.{js,jsx,ts,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
